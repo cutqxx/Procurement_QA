d7c3788d55782dd96cdb77c5b5aa49f612d5d06c
cutqxx <cutqxx@gmail.com>
2022-04-22 17:45:06 +0300

    win 10

--- test_project.py
+++ test_project.py
@@ -1,13 +1,13 @@
 import pytest
 from .models.project_page import ProjectPage
-from.models.sheet_page import SheetPage
+from .models.sheet_page import SheetPage
 from playwright.sync_api import Page
 import time
 
 
 @pytest.mark.smoke
 class TestSmoke():
-    def test_create_new_project(self, page:Page):
+    def test_create_new_project(self, page: Page):
         link = "https://procurement2021.tk/projects"
         page_project = ProjectPage(page, link)
         page_project.open()
@@ -17,7 +17,7 @@ class TestSmoke():
         page_project.create_new_projects()
         time.sleep(1)
 
-    def test_add_potreb_in_project(self, page:Page):
+    def test_add_potreb_in_project(self, page: Page):
         link = "https://procurement2021.tk/projects"
         page_project = ProjectPage(page, link)
         page_sheet = SheetPage(page, link)
@@ -28,16 +28,15 @@ class TestSmoke():
         # time.sleep(3)
 
     @pytest.mark.new
-    def test_fill_forma_prorabotka(self, page:Page):
-            link = "https://procurement2021.tk/projects"
-            page_project = ProjectPage(page, link)
-            page_sheet = SheetPage(page, link)
-            # page_project.open_context()
-            print("Тест: заполнение данных в форму предварительной подготовки")
-            page_project.open()
-            page_project.open_project()
-            page_sheet.click_button_open_forma(1)
-            page_sheet.add_pid_in_forma()
-            page_sheet.fill_the_fields_prorabotka_potrebnosti()
-            page_sheet.click_button_vnesti_in_forma()
-
+    def test_fill_forma_prorabotka(self, page: Page):
+        link = "https://procurement2021.tk/projects"
+        page_project = ProjectPage(page, link)
+        page_sheet = SheetPage(page, link)
+        # page_project.open_context()
+        print("Тест: заполнение данных в форму предварительной подготовки")
+        page_project.open()
+        page_project.open_project()
+        page_sheet.click_button_open_forma(1)
+        page_sheet.add_pid_in_forma()
+        page_sheet.fill_the_fields_prorabotka_potrebnosti()
+        page_sheet.click_button_vnesti_in_forma()
